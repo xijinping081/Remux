5cdda2be088b7acef4a280fe025842a0467235d4
Joel Fernandes <joelaf@google.com>
2018-06-30 17:17:24 +0000

    sched: fair: Skip frequency updates if CPU about to idle

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3569,7 +3569,7 @@ update_cfs_rq_load_avg(u64 now, struct cfs_rq *cfs_rq, bool update_freq)
 
 #define UPDATE_TG	0x0
 #define SKIP_AGE_LOAD	0x0
-#define SKIP_CPUFREQ	0x3
+#define SKIP_CPUFREQ	0x0
 
 static inline void update_load_avg(struct sched_entity *se, int not_used1)
 {
@@ -3796,7 +3796,6 @@ static void
 dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 {
 	int update_flags;
-
 	/*
 	 * Update run-time statistics of the 'current'.
 	 */
