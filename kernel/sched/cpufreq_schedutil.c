f6ef761e4c485e5fd6960336000096e59f91352e
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-07-03 09:15:58 +0530

    cpufreq: schedutil: Fix for limits update with fast switch enabled
    When the policy limits are applied with fast switch enabled, the
    policy->cur is not updated. This can result in incorrect calculation
    of the average capacity and any subsequent limit updates.
    
    Update cpufreq_policy_apply_limits_fast() API to return a non-zero
    value when the frequency is updated. Make use of this return value
    and update policy->cur. While at it, print cpu_frequency trace point,
    when frequency is changed due to change in limits.
    
    Change-Id: I51732fa061aac11231d1f18ca70f31f252f0a0dd
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1093,6 +1093,8 @@ static void sugov_limits(struct cpufreq_policy *policy)
 {
 	struct sugov_policy *sg_policy = policy->governor_data;
 	unsigned long flags;
+	unsigned int ret;
+	int cpu;
 
 	if (!policy->fast_switch_enabled) {
 		mutex_lock(&sg_policy->work_lock);
@@ -1103,7 +1105,12 @@ static void sugov_limits(struct cpufreq_policy *policy)
 		cpufreq_policy_apply_limits(policy);
 		mutex_unlock(&sg_policy->work_lock);
 	} else {
-		cpufreq_policy_apply_limits_fast(policy);
+		ret = cpufreq_policy_apply_limits_fast(policy);
+		if (ret && policy->cur != ret) {
+			policy->cur = ret;
+			for_each_cpu(cpu, policy->cpus)
+				trace_cpu_frequency(ret, cpu);
+		}
 	}
 
 	sg_policy->limits_changed = true;
