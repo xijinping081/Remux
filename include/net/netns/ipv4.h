603312490a04feabcac36a2b90b5ad368768bedd
Maciej Żenczykowski <maze@google.com>
2020-05-15 09:20:37 -0700

    ANDROID: namespace'ify tcp_default_init_rwnd implementation
    Not needed in 4.19+ since this Android specific sysctl was
    not included in later kernels.
    
    Test: via uml net tests with namespaces enabled
    Bug: 149894399
    Bug: 166680775
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: I2ad1dbc977d40ee260bde23c6ed32f2706082660
    (cherry picked with simple conflict resolution fixups from
     android-4.14-stable commit 08870bd1a24fc7f3ae4ff30bc7e64c09edd931d4,
     retested via:
       ARCH=um SUBARCH=x86_64 /aosp-tests/net/test/run_net_test.sh --builder all_tests.py
     with patch to make adb detection, ie. HasEstablishedTcpSessionOnPort() always true)

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -112,6 +112,7 @@ struct netns_ipv4 {
 	int sysctl_tcp_orphan_retries;
 	int sysctl_tcp_fin_timeout;
 	unsigned int sysctl_tcp_notsent_lowat;
+	int sysctl_tcp_default_init_rwnd;
 	int sysctl_tcp_plb_enabled;
 	int sysctl_tcp_plb_idle_rehash_rounds;
 	int sysctl_tcp_plb_rehash_rounds;
