fb45ecb73542287001dfc34facccc2e0ffe6e219
Wei Wang <wvw@google.com>
2020-06-30 13:47:39 -0700

    cpufreq: schedutil: Maintain raw cache when next_f is not changed

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -46,6 +46,7 @@ struct sugov_policy {
 	unsigned long avg_cap;
 	unsigned int next_freq;
 	unsigned int cached_raw_freq;
+	unsigned int prev_cached_raw_freq;
 	unsigned long hispeed_util;
 	unsigned long max;
 
@@ -169,8 +170,8 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 		return;
 
 	if (sugov_up_down_rate_limit(sg_policy, time, next_freq)) {
-		/* Reset cached freq as next_freq isn't changed */
-		sg_policy->cached_raw_freq = 0;
+		/* Restore cached freq as next_freq is not changed */
+		sg_policy->cached_raw_freq = sg_policy->prev_cached_raw_freq;
 		return;
 	}
 
@@ -225,6 +226,7 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 		return sg_policy->next_freq;
 
 	sg_policy->need_freq_update = false;
+	sg_policy->prev_cached_raw_freq = sg_policy->cached_raw_freq;
 	sg_policy->cached_raw_freq = freq;
 	return cpufreq_driver_resolve_freq(policy, freq);
 }
@@ -432,8 +434,8 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 		if (busy && next_f < sg_policy->next_freq) {
 			next_f = sg_policy->next_freq;
 
-			/* Reset cached freq as next_freq has changed */
-			sg_policy->cached_raw_freq = 0;
+			/* Restore cached freq as next_freq has changed */
+			sg_policy->cached_raw_freq = sg_policy->prev_cached_raw_freq;
 		}
 	}
 
@@ -1033,9 +1035,11 @@ static int sugov_start(struct cpufreq_policy *policy)
 	sg_policy->limits_changed = false;
 	sg_policy->need_freq_update = false;
 	sg_policy->cached_raw_freq = 0;
+	sg_policy->prev_cached_raw_freq = 0;
 #ifdef VENDOR_EDIT
 	sg_policy->flags	= 0;
 #endif
+
 	for_each_cpu(cpu, policy->cpus) {
 		struct sugov_cpu *sg_cpu = &per_cpu(sugov_cpu, cpu);
 
