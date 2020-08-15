402ff9d91e5a6c15b0ab597e7800d1638d4cc03f
Nick Terrell <terrelln@fb.com>
2020-08-14 17:30:10 -0700

    lz4: fix kernel decompression speed
    This patch replaces all memcpy() calls with LZ4_memcpy() which calls
    __builtin_memcpy() so the compiler can inline it.
    
    LZ4 relies heavily on memcpy() with a constant size being inlined.  In x86
    and i386 pre-boot environments memcpy() cannot be inlined because memcpy()
    doesn't get defined as __builtin_memcpy().
    
    An equivalent patch has been applied upstream so that the next import
    won't lose this change [1].
    
    I've measured the kernel decompression speed using QEMU before and after
    this patch for the x86_64 and i386 architectures.  The speed-up is about
    10x as shown below.
    
    Code	Arch	Kernel Size	Time	Speed
    v5.8	x86_64	11504832 B	148 ms	 79 MB/s
    patch	x86_64	11503872 B	 13 ms	885 MB/s
    v5.8	i386	 9621216 B	 91 ms	106 MB/s
    patch	i386	 9620224 B	 10 ms	962 MB/s
    
    I also measured the time to decompress the initramfs on x86_64, i386, and
    arm.  All three show the same decompression speed before and after, as
    expected.
    
    [1] https://github.com/lz4/lz4/pull/890
    
    Signed-off-by: Nick Terrell <terrelln@fb.com>
    Change-Id: I5764cb0c570fb384355d1e6d4669b415eb88c516
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Yann Collet <yann.collet.73@gmail.com>
    Cc: Gao Xiang <gaoxiang25@huawei.com>
    Cc: Sven Schmidt <4sschmid@informatik.uni-hamburg.de>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Arvind Sankar <nivedita@alum.mit.edu>
    Link: http://lkml.kernel.org/r/20200803194022.2966806-1-nickrterrell@gmail.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    (cherry picked from commit 79cb95ee6f19213938de8ca61403a51d8bd4d5c2)
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Kanishk <kanishkthederp@gmail.com>
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- lib/lz4/lz4_decompress.c
+++ lib/lz4/lz4_decompress.c
@@ -347,7 +347,7 @@ _copy_match:
 				size_t const copySize = (size_t)(lowPrefix - match);
 				size_t const restSize = length - copySize;
 
-				memcpy(op, dictEnd - copySize, copySize);
+				LZ4_memcpy(op, dictEnd - copySize, copySize);
 				op += copySize;
 				if (restSize > (size_t)(op - lowPrefix)) {
 					/* overlap copy */
@@ -357,7 +357,7 @@ _copy_match:
 					while (op < endOfMatch)
 						*op++ = *copyFrom++;
 				} else {
-					memcpy(op, lowPrefix, restSize);
+					LZ4_memcpy(op, lowPrefix, restSize);
 					op += restSize;
 				}
 			}
