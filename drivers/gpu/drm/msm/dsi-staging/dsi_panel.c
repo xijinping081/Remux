cc930cf13ad2f5acb14eec217517c4dfa5129e1b
balgxmr <jose@pixelos.net>
2023-05-07 12:38:42 -0500

    drm/msm/dsi-staging: Kill doze logging
    Link: https://github.com/balgxmr/kernel_xiaomi_cepheus/commit/c5dc65e5a7d92002eaebd841f142299194ec6dde#diff-6caee3ce26577c0e33ea4ef5748e0032ea51021d68038cad45e2774f9e2c621f
    Signed-off-by: balgxmr <jose@pixelos.net>
    Change-Id: Iaf8321cc6c4813fc088ea30bde00068d188ac6d3
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1182,7 +1182,7 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 	#endif /* VENDOR_EDIT */
 	rc = mipi_dsi_dcs_set_display_brightness(dsi, bl_lvl);
 	if (rc < 0)
-		pr_err("failed to update dcs backlight:%d\n", bl_lvl);
+		pr_debug("failed to update dcs backlight:%d\n", bl_lvl);
 
 #ifdef VENDOR_EDIT
 	oppo_panel_process_dimming_v2_post(panel, false);
