fd050d55714da081d109c35d5dc443c9b42d7efb
Masahito S <firelzrd@gmail.com>
2024-08-04 00:40:03 +0900

    sched: bore: Rename renice_task() -> reweight_task_by_prio()
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -834,7 +834,7 @@ static inline u64 scale_slice(u64 delta, struct sched_entity *se) {
 static void reweight_entity(
 	struct cfs_rq *cfs_rq, struct sched_entity *se, unsigned long weight);
 
-static void renice_task(struct task_struct *p, int prio)
+static void reweight_task_by_prio(struct task_struct *p, int prio)
 {
 	struct sched_entity *se = &p->se;
 	struct cfs_rq *cfs_rq = cfs_rq_of(se);
@@ -858,7 +858,7 @@ static void update_burst_score(struct sched_entity *se) {
 
 	new_prio = min(39, prio + se->burst_score);
 	if (new_prio != prev_prio)
-	 	renice_task(p, new_prio);
+	 	reweight_task_by_prio(p, new_prio);
 }
 
 static void update_burst_penalty(struct sched_entity *se) {
