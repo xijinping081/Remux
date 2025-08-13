1c948aeebd3862a72ddb2016048338bf746643e3
mizukanaAlt <rina301087@gmail.com>
2025-08-13 03:28:32 +0000

    more disable logs spam

--- drivers/devfreq/governor_passive.c
+++ drivers/devfreq/governor_passive.c
@@ -114,8 +114,7 @@ static int update_devfreq_passive(struct devfreq *devfreq, unsigned long freq)
 
 	if (devfreq->profile->freq_table
 		&& (devfreq_update_status(devfreq, freq)))
-		dev_err(&devfreq->dev,
-			"Couldn't update frequency transition information.\n");
+		//dev_err(&devfreq->dev, "Couldn't update frequency transition information.\n");
 
 	devfreq->previous_freq = freq;
 
