0a47d094a794ab7dc50630a4403ea38ba8caae59
Hypergang02 <158050493+Hypergang02@users.noreply.github.com>
2025-09-03 22:51:42 +0700

    staging: android: ion: fix miss merge

--- drivers/staging/android/ion/ion_system_heap.c
+++ drivers/staging/android/ion/ion_system_heap.c
@@ -143,9 +143,6 @@ static struct page *alloc_buffer_page(struct ion_system_heap *heap,
 	if (!page)
 		return 0;
 
-	if (page && cached)
-		ion_pages_sync_for_device(NULL, page, PAGE_SIZE << order,
-					  DMA_BIDIRECTIONAL);
 	return page;
 }
 
