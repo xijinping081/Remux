14be4d6fd52ee5a5a6a7bb24ca2a7b03928928a9
Sorayukii <sorayukii69@gmail.com>
2025-09-05 10:52:34 +0000

    sched: Introduce BORE Scheduler (5.1.0)
    BORE (Burst-Oriented Response Enhancer) is enhanced versions of CFS (Completely Fair Scheduler)
    and EEVDF (Earliest Eligible Virtual Deadline First) Linux schedulers. Developed with the aim of
    maintaining these schedulers' high performance while delivering resilient responsiveness to user
    input under as versatile load scenario as possible.
    
    taken from https://github.com/firelzrd/bore-scheduler/tree/main/patches/legacy/linux-4.19-bore
    
    0001-linux4.19.y-bore5.1.0.patch
    0002-constgran-vanilla-max.patch
    0002-sched-ext-coexistence-fix.patch
    
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1698,6 +1698,10 @@ extern void init_sched_dl_class(void);
 extern void init_sched_rt_class(void);
 extern void init_sched_fair_class(void);
 
+#ifdef CONFIG_SCHED_BORE
+extern void reweight_task(struct task_struct *p, int prio);
+#endif
+
 extern void resched_curr(struct rq *rq);
 extern void resched_cpu(int cpu);
 
