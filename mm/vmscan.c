7cbb3d8a96b72253af87edd2401d3d65f5dd6b39
Sorayukii <sorayukii69@gmail.com>
2025-11-21 07:36:35 +0700

    mm: Refactor sysctl knobs for protecting the working set
    changes:
    - anon_min_kbytes -> anon_min_ratio
    - clean_low_kbytes -> clean_low_ratio
    - clean_min_kbytes -> clean_min_ratio
    
    The main purpose is to make tuning easier by using a ratio based on total memory instead of directly using a value in kbytes.
    
    this idea from le9uo, i just make it working in kernel 4.9 without mglru support. If your kernel have mglru use le9uo.
    
    reference: https://github.com/firelzrd/le9uo
    
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -160,9 +160,13 @@ unsigned long sysctl_clean_min_kbytes __read_mostly = CONFIG_CLEAN_MIN_KBYTES;
 #define prefetchw_prev_lru_page(_page, _base, _field) do { } while (0)
 #endif
 
-unsigned long sysctl_anon_min_kbytes __read_mostly = CONFIG_ANON_MIN_KBYTES;
-unsigned long sysctl_clean_low_kbytes __read_mostly = CONFIG_CLEAN_LOW_KBYTES;
-unsigned long sysctl_clean_min_kbytes __read_mostly = CONFIG_CLEAN_MIN_KBYTES;
+unsigned int sysctl_anon_min_ratio  __read_mostly = CONFIG_ANON_MIN_RATIO;
+unsigned int sysctl_clean_low_ratio __read_mostly = CONFIG_CLEAN_LOW_RATIO;
+unsigned int sysctl_clean_min_ratio __read_mostly = CONFIG_CLEAN_MIN_RATIO;
+static u64 sysctl_anon_min_ratio_kb  __read_mostly = 0;
+static u64 sysctl_clean_low_ratio_kb __read_mostly = 0;
+static u64 sysctl_clean_min_ratio_kb __read_mostly = 0;
+static u64 workingset_protection_prev_totalram __read_mostly = 0;
 
 /*
  * From 0 .. 100.  Higher means more swappy.
@@ -2221,30 +2225,78 @@ static unsigned long shrink_list(enum lru_list lru, unsigned long nr_to_scan,
 	return shrink_inactive_list(nr_to_scan, lruvec, sc, lru);
 }
 
+int vm_workingset_protection_update_handler(struct ctl_table *table, int write,
+		void __user *buffer, size_t *lenp, loff_t *ppos)
+{
+	/* Update sysctl value and enforce min/max constraints */
+	int ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
+	if (ret || !write)
+		return ret;
+
+	/* Recalculation on next scan */
+	workingset_protection_prev_totalram = 0;
+
+	return 0;
+}
+
 static void prepare_workingset_protection(pg_data_t *pgdat, struct scan_control *sc)
 {
+	unsigned long node_mem_total;
+	struct sysinfo i;
+
+	/*
+	 * Update working set protection thresholds if
+	 * sysctl ratios are configured.
+	 */
+	if (likely(sysctl_anon_min_ratio  ||
+	           sysctl_clean_low_ratio ||
+		       sysctl_clean_min_ratio)) {
+
+		/* Get total memory of the current NUMA node */
+#ifdef CONFIG_NUMA
+		si_meminfo_node(&i, pgdat->node_id);
+#else
+		si_meminfo(&i);
+#endif
+		node_mem_total = i.totalram;
+
+		/*
+		 * Recalculate ratio thresholds only if memory size changes
+		 * and convert percentage ratio into kbytes.
+		 */
+		if (unlikely(workingset_protection_prev_totalram != node_mem_total)) {
+			sysctl_anon_min_ratio_kb  =
+				node_mem_total * sysctl_anon_min_ratio  / 100;
+			sysctl_clean_low_ratio_kb =
+				node_mem_total * sysctl_clean_low_ratio / 100;
+			sysctl_clean_min_ratio_kb =
+				node_mem_total * sysctl_clean_min_ratio / 100;
+			workingset_protection_prev_totalram = node_mem_total;
+		}
+	}
+
 	/*
 	 * Check the number of anonymous pages to protect them from
 	 * reclaiming if their amount is below the specified.
 	 */
-	if (sysctl_anon_min_kbytes) {
+	if (sysctl_anon_min_ratio) {
 		unsigned long reclaimable_anon;
 
 		reclaimable_anon =
 			node_page_state(pgdat, NR_ACTIVE_ANON) +
 			node_page_state(pgdat, NR_INACTIVE_ANON) +
 			node_page_state(pgdat, NR_ISOLATED_ANON);
-		reclaimable_anon <<= (PAGE_SHIFT - 10);
 
-		sc->anon_below_min = reclaimable_anon < sysctl_anon_min_kbytes;
-	} else
+		sc->anon_below_min = reclaimable_anon < sysctl_anon_min_ratio_kb;
+	} else {
 		sc->anon_below_min = 0;
+	}
 
 	/*
 	 * Check the number of clean file pages to protect them from
 	 * reclaiming if their amount is below the specified.
 	 */
-	if (sysctl_clean_low_kbytes || sysctl_clean_min_kbytes) {
+	if (sysctl_clean_low_ratio || sysctl_clean_min_ratio) {
 		unsigned long reclaimable_file, dirty, clean;
 
 		reclaimable_file =
@@ -2257,12 +2309,12 @@ static void prepare_workingset_protection(pg_data_t *pgdat, struct scan_control 
 		 * all the values are not read at once.
 		 */
 		if (likely(reclaimable_file > dirty))
-			clean = (reclaimable_file - dirty) << (PAGE_SHIFT - 10);
+			clean = reclaimable_file - dirty;
 		else
 			clean = 0;
 
-		sc->clean_below_low = clean < sysctl_clean_low_kbytes;
-		sc->clean_below_min = clean < sysctl_clean_min_kbytes;
+		sc->clean_below_low = clean < sysctl_clean_low_ratio_kb;
+		sc->clean_below_min = clean < sysctl_clean_min_ratio_kb;
 	} else {
 		sc->clean_below_low = 0;
 		sc->clean_below_min = 0;
@@ -2494,22 +2546,12 @@ out:
 
 		/*
 		 * Hard protection of the working set.
+		 * Don't reclaim anon/file pages when the amount is
+		 * below the watermark of the same type.
 		 */
-		if (file) {
-			/*
-			 * Don't reclaim file pages when the amount of
-			 * clean file pages is below vm.clean_min_kbytes.
-			 */
-			if (sc->clean_below_min)
-				scan = 0;
-		} else {
-			/*
-			 * Don't reclaim anonymous pages when their
-			 * amount is below vm.anon_min_kbytes.
-			 */
-			if (sc->anon_below_min)
-				scan = 0;
-		}
+		if (file ? sc->clean_below_min : sc->anon_below_min)
+			scan = 0;
+
 		*lru_pages += size;
 		nr[lru] = scan;
 	}
