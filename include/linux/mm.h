03fbfcdee673d1a6a14cfe1df088d16564e9f13f
Sorayukii <sorayukii69@gmail.com>
2025-11-21 07:36:35 +0700

    mm: Refactor sysctl knobs for protecting the working set
    changes:
    - anon_min_kbytes -> anon_min_ratio
    - clean_low_kbytes -> clean_low_ratio
    - clean_min_kbytes -> clean_min_ratio
    
    The main purpose is to make tuning easier by using a ratio based on total memory instead of directly using a value in kbytes.
    
    this idea from le9uo, i just make it working in kernel 4.9 without mglru support. If your kernel have mglru use le9uo.
    
    reference: https://github.com/firelzrd/le9uo
    
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -137,9 +137,12 @@ extern int mmap_rnd_compat_bits __read_mostly;
 
 extern int sysctl_max_map_count;
 
-extern unsigned long sysctl_anon_min_kbytes;
-extern unsigned long sysctl_clean_low_kbytes;
-extern unsigned long sysctl_clean_min_kbytes;
+extern unsigned int sysctl_anon_min_ratio;
+extern unsigned int sysctl_clean_low_ratio;
+extern unsigned int sysctl_clean_min_ratio;
+int vm_workingset_protection_update_handler(
+	struct ctl_table *table, int write,
+	void __user *buffer, size_t *lenp, loff_t *ppos);
 
 extern unsigned long sysctl_user_reserve_kbytes;
 extern unsigned long sysctl_admin_reserve_kbytes;
