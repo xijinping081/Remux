750151cc9a0a03c33db6d1bca59ccadc628df7e6
Sorayukii <sorayukii69@gmail.com>
2026-03-09 17:34:19 +0700

    cpufreq: schedutil: Enable iowait boost by default
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -873,7 +873,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->up_rate_limit_us = 0;
 	tunables->down_rate_limit_us = 0;
 
-	tunables->iowait_boost_enable = false;
+	/* Enable iowait_boost by default */
+	tunables->iowait_boost_enable = true;
 
 	/* Disable exponential frequency scaling by default */
 	tunables->exp_util = false;
