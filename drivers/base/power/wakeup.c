0972bc89b974bf4721c337e5cc2a5671f84f676b
Sorayukii <sorayukii69@gmail.com>
2026-03-14 17:11:55 +0700

    boeffla_wl_blocker: Enable wl_blocker_active by default
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -101,7 +101,7 @@ static spinlock_t statistics_lock;
 #include "boeffla_wl_blocker.h"
 
 char list_wl_search[LENGTH_LIST_WL_SEARCH] = {0};
-bool wl_blocker_active = false;
+bool wl_blocker_active = true;
 bool wl_blocker_debug = false;
 
 static void wakeup_source_deactivate(struct wakeup_source *ws);
