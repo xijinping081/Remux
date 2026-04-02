434b39c53618ff6fd1f99da0206cdfbf741fae77
Sorayukii <sorayukii69@gmail.com>
2026-04-02 18:02:08 +0700

    tcp: Force the system to prioritize low latency
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -87,7 +87,7 @@
 #include <net/oplus_nwpower.h>
 //#endif /* OPLUS_FEATURE_NWPOWER */
 int sysctl_tcp_tw_reuse __read_mostly;
-int sysctl_tcp_low_latency __read_mostly;
+int sysctl_tcp_low_latency __read_mostly = 1;
 
 #ifdef CONFIG_TCP_MD5SIG
 static int tcp_v4_md5_hash_hdr(char *md5_hash, const struct tcp_md5sig_key *key,
